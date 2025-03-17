88b41a72c92039501892418fde145a104296780a
Nikhil Idiculla <nikhilidiculla@gmail.com>
2025-03-16 21:46:42 -0700

    Init basic project

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
